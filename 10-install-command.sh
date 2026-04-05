ee697a50a50eb86e15a0248277651f9a1a8108cc
BharathKumarReddy <nbkumar2103@gmail.com>
2026-04-05 17:34:18 +0530

    updated file

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -10,7 +10,7 @@ else
     echo "You are running with root access"
 fi
 
-dnf list installed mysql -y
+dnf list install mysql -y
 
 # check already installed or not. if installed $? is 0, then
 # if not installed $? is not 0. expression is true
